50e1279e0d4bf6853cbda6718d15e031f6bde782
Lem <natsouscor@gmail.com>
2025-12-12 17:12:12 +0100

    test

--- string.h
+++ string.h
@@ -3,18 +3,24 @@
 class string {
     public:
 
-        int length() const noexcept;
-        int max_size() const noexcept;
-        void resize (int n);
-        void resize (int n, char c);
-        string& operator= (const string& str);
+        size_t length() const noexcept;
+        size_t max_size() const noexcept;
+        void resize (size_t n);
+        void resize (size_t n, char c);
+
+        string& operator=(const string& str);
+        
+        string operator+(const string& lhs, char rhs);
+        string operator+(string&& lhs, char rhs);
+        string operator+(char lhs, const string& rhs);
+        string operator+(char lhs, string&& rhs);
 
         
 
     private:
         char* str_;
-        int size_;
-        int capacity_;
-        static int max_size_;
+        size_t size_;
+        size_t capacity_;
+        static size_t max_size_;
 
 };
