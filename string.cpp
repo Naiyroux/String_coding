ceb4193de372f0a9b38459f4a70e4d6b6e998b8d
Lem <natsouscor@gmail.com>
2025-12-12 17:55:25 +0100

    Ajout du debut des fonctions de B

--- string.cpp
+++ string.cpp
@@ -1,1 +1,43 @@
 #include "string.h"
+#include<cstdlib>
+
+
+size_t string::length(){
+    return size_t size_;
+};
+
+size_t string::max_size(){
+    return size_t max_size_;
+};
+
+void string::resize (size_t n){
+    if (n > max_size_){
+        // return error, à modifier
+        return ;
+    }
+    elif (n < size_){
+        size_ =  n;
+    }
+};
+
+void string::resize (size_t n, char c){
+    if (n > max_size_){
+        // return error, à modifier
+        return ;
+    }
+    elif (n < size_){
+        size_ =  n;
+    }
+    else{
+        for (int i = size_; i < n;i++){
+            
+        }
+    }
+};
+
+string& string::operator=(const string& str);
+
+string string::operator+(const string& lhs, char rhs);
+string string::operator+(string&& lhs, char rhs);
+string string::operator+(char lhs, const string& rhs);
+string string::operator+(char lhs, string&& rhs);
