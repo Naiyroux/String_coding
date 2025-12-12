ee567dacc73c24a824fc36230960c4bd9152afdc
jadehusson <jadehusson96@gmail.com>
2025-12-12 17:53:37 +0100

    Update string.h

--- string.h
+++ string.h
@@ -3,20 +3,27 @@
 class string {
     public:
     	string (const string& str);
-        string& operator= (char c);
-        string operator+ (const string& lhs, const char*   rhs);
-        const char* c_str() const noexcept;
-        size_t size() const noexcept;
-        void clear() noexcept;
-        size_t length() const noexcept;
-        size_t max_size() const noexcept;
-        void resize (size_t n);
-        void resize (size_t n, char c);
-        string& operator=(const string& str);
-        string operator+(const string& lhs, char rhs);
-        string operator+(string&& lhs, char rhs);
-        string operator+(char lhs, const string& rhs);
-        string operator+(char lhs, string&& rhs);
+     ~string();
+ 
+     const char* c_str() const;
+     bool empty() const ;
+     void reserve (size_t size_);
+     size_t size() const;
+     void clear() noexcept;
+     size_t length() const;
+     size_t max_size() const;
+     size_t capacity() const (size_t n);
+     void resize (size_t n);
+     void resize (size_t n, char c);
+     string& operator= (char c);
+     string& operator=(const char*);
+     string& operator=(const string& str);
+     friend string operator+ (const string& lhs, const char* rhs);
+     friend string operator+ (const string& lhs, const string& rhs);
+     friend string operator+(const string& lhs, char rhs);
+     friend string operator+(string&& lhs, char rhs);
+     friend string operator+(char lhs, const string& rhs);
+     friend string operator+(char lhs, string&& rhs);
 
         
 
@@ -26,3 +33,4 @@ class string {
         size_t capacity_;
         static size_t max_size_ = 100;
 }
+
