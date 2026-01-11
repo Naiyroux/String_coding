55f46ce3897d5a208a1c19b30cefa448ba4b25ea
Lem <natsouscor@gmail.com>
2026-01-11 23:14:44 +0100

    Ajout basiques

--- string.cpp
+++ string.cpp
@@ -1,5 +1,8 @@
 #include "string.h"
 #include<cstdlib>
+#include <stdexcept> // Pour les exceptions (length_error)
+
+size_t string::max_size_ = 100;
 
 //Constructeur Copie 
 string::string(const string& str) {
@@ -38,43 +41,69 @@ string& string::operator=(char c) {
 
 // je n'ai pas réussi la fonction string operator+(const string& lhs, const char* rhs)
 
+// Constructeur par défaut
+string::string() : size_(0), capacity_(0) {
+    str_ = new char[1];
+    str_[0] = '\0';
+}
 
-size_t string::length(){
-    return size_t size_;
+string::string(const char* str) {
+    if (str == nullptr) {
+        size_ = 0;
+        capacity_ = 0;
+        str_ = new char[1];
+        str_[0] = '\0';
+    } else {
+        size_t len = 0;
+        while (str[len] != '\0') {
+            len++;
+        }
+        size_ = len;
+        capacity_ = len;
+        str_ = new char[capacity_ + 1];
+        for (size_t i = 0; i < size_; i++) {
+            str_[i] = str[i];
+        }
+        str_[size_] = '\0';
+        }
+    }
+
+size_t string::length() const noexcept{
+    return size_;
 };
 
-size_t string::max_size(){
-    return size_t max_size_;
+size_t string::max_size() const noexcept{
+    return max_size_;
 };
 
 void string::resize (size_t n){
     if (n > max_size_){
         // return error, à modifier
-        return std::length_error;
+        throw std::length_error("String too long");
     }
-    elif (n < size_){
+    else if (n < size_){
         size_ =  n;
     }
-};  return 1;
+}
 
 void string::resize (size_t n, char c){
     if (n > max_size_){
-        // return error, à modifier
-        return ;
+
+        
     }
-    elif (n <= size_){
+    if (n <= size_){
         size_ =  n;
-        data_[size_] = '\0';
+        str_[size_] = '\0';
     }
-    else{
+    else if (n > size_){
         if (n > capacity_){
-            reserve(n)
+            reserve(n);
         }
         for (size_t i = size_; i < n;i++){
                 str_[i] = c;
         }
         size_ =  n;
-        data_[size_] = '\0';
+        str_[size_] = '\0';
         
     }
 }
@@ -86,41 +115,71 @@ string& string::operator=(const string& str){
     else{
         delete[] str_;
 
-        length = str.length;
-        str_ =  new char[length+1];
+        size_ = str.size_;
+        str_ =  new char[capacity_  +1];
 
-        for (size_t i = 0; i<length ; i++){
+        for (size_t i = 0; i < size_ ; i++){
             str_[i] = str.str_[i];
         }
-        str_[length] = '\0';
+        str_[size_] = '\0';
         return *this;
     }
 }
 
-string string::operator+(const string& lhs, char rhs){
+string string::operator+(const char* rhs)const{
     string newstr;
+
+    size_t rhs_length = 0;
+
+    while (rhs[rhs_length]!='\0'){
+        rhs_length++;
+    }
     
-    newstr.length = lhs.length + rhs.length;
+    newstr.size_ = this->size_ + rhs_length;
 
-    if (n > max_size_){
-        // return error, à modifier
-        return ;
+    if (newstr.size_ > max_size_){
+
+        throw std::length_error("String too long");
     }
 
-    newstr.str_ = new char[newstr.length + 1]
+    newstr.str_ = new char[newstr.size_ + 1];
 
-    for (size_t i = 0; i < lhs.length; i++){
-        newstr.str_[i] = lhs.str_[i];
+    for (size_t i = 0; i < this->size_ ; i++){
+        newstr.str_[i] = this->str_[i];
     }
 
-    for (size_t j = 0; j < rhs.length; j++){
-        newstr.str_[lhs.length + j] = rhs.str_[j];
+    for (size_t j = 0; j < rhs_length; j++){
+        newstr.str_[this->size_ + j] = rhs[j];
     }
 
-    newstr_[newstr.length] = '\0';
+    newstr.str_[newstr.size_] = '\0';
     return newstr;
 
 }
+
+string string::operator+(char rhs)const{
+    string newstr;
+    
+    newstr.size_ = this->size_ + 1;
+
+    if (newstr.size_ > max_size_){
+
+        throw std::length_error("String too long");
+    }
+
+    newstr.str_ = new char[newstr.size_ + 1];
+
+    for (size_t i = 0; i < this->size_; i++){
+        newstr.str_[i] = this->str_[i];
+    }
+
+    newstr.str_[this->size_] = rhs;
+    newstr.str_[newstr.size_] = '\0';
+
+    return newstr;
+
+}
+/*
 string string::operator+(string&& lhs, char rhs);
 string string::operator+(char lhs, const string& rhs);
 
@@ -130,8 +189,8 @@ string::~string() {
 	delete [] this->values;
 }
 
-bool empty() const {
-	if (str.length() == 0) {
+bool string::empty() const {
+	if (size_ == 0) {
 		return True;
 	}
 	else {
@@ -142,4 +201,4 @@ bool empty() const {
 
 string operator+ (const string& lhs, const string& rhs){
 	return lhs + rhs;
-}
+}*/
--- string.h
+++ string.h
@@ -1,38 +1,45 @@
 #ifndef MY_STRING_H
 #define MY_STRING_H
+#include <cstddef>
+using std::size_t;
  
 class string {
     public:
+     string ();
      string (const string& str);
+     string(const char* str);
      int test;
      ~string();
      const char* c_str() const noexcept;
-     bool empty() const ;
+     bool empty() const;
      void reserve (size_t n);
      size_t size() const noexcept;
      void clear() noexcept;
      size_t length() const noexcept;
      size_t max_size() const noexcept;
-     size_t capacity() const (size_t n);
+     size_t capacity() const;
      void resize (size_t n);
      void resize (size_t n, char c);
      string& operator= (char c);
      string& operator=(const char*);
      string& operator=(const string& str);
+     /*
      friend string operator+ (const string& lhs, const char* rhs);
      friend string operator+ (const string& lhs, const string& rhs);
      friend string operator+(const string& lhs, char rhs);
      friend string operator+(string&& lhs, char rhs);
      friend string operator+(char lhs, const string& rhs);
      friend string operator+(char lhs, string&& rhs);
-
+    */
+     string operator+(const char* rhs)const;
+     string operator+(char rhs)const;
         
 
     private:
         char* str_;
         size_t size_;
         size_t capacity_;
-        static size_t max_size_ = 100;
+        static size_t max_size_;
 };
 
 #endif
